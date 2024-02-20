5e9e057c6b248eb324dc529d42cd1cba553c6621
laguto <Nathan.wigley@hotmail.com>
2024-02-20 23:46:27 +0800

    made some changes

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('mouseover', () => link.style.textDecoration = 'underline');
-    link.addEventListener('mouseout', () => link.style.textDecoration = 'none');
+document.querySelectorAll('.social-links a').forEach(link => {
+    link.addEventListener('mouseover', () => {
+        console.log('Mouse over');
+        link.querySelector('.social-logo').classList.add('hovered');
+    });
+    link.addEventListener('mouseout', () => {
+        console.log('Mouse out');
+        link.querySelector('.social-logo').classList.remove('hovered');
+    });
 });
